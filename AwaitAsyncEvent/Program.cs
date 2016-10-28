b42e95bcc9b227657c4752d84ada4db11af43a7a
房懷安 <liddle.fang@gmail.com>
2016-10-28 13:26:56 +0800

    initial project

---
+++ AwaitAsyncEvent/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace AwaitAsyncEvent
+{
+	class MainClass
+	{
+		public static void Main (string [] args)
+		{
+			var url = "http://tripim.com";
+
+			WriteLine (url);
+
+			// Event
+			var workerEvent = new EventWebWorker ();
+
+			workerEvent.HtmlStringReceived += (object sender, HtmlReceivedEventArgs e) => {
+				WriteLine ($"event html:{ e.Html }");
+			};
+
+			workerEvent.DownloadHtmlStringWithEvent (url);
+
+
+			// async, await
+			var workerAsync = new AsyncWebWorker ();
+
+			Task.Run (async () => { 
+				var html = await workerAsync.DownloadHtmlStringAsync (url);
+				WriteLine ($"async html:{ html }");
+			} );
+
+			ReadLine ();
+		}
+	}
+
+	public class EventWebWorker
+	{
+		private WebClient MyWebClient { get; set; }
+
+		public EventWebWorker ()
+		{
+			MyWebClient = new WebClient ();
+		}
+
+		public void DownloadHtmlStringWithEvent (string url)
+		{
+			var html = MyWebClient.DownloadString (url);
+
+			EventHandler<HtmlReceivedEventArgs> handler = HtmlStringReceived;
+			var args = new HtmlReceivedEventArgs { Html = html };
+			if (null != handler) {
+				handler (this, args);
+			}
+		}
+		public event EventHandler<HtmlReceivedEventArgs> HtmlStringReceived;
+	}
+	public class HtmlReceivedEventArgs : EventArgs
+	{
+		public string Html { get; set; }
+	}
+
+	public class AsyncWebWorker
+	{
+		private WebClient MyWebClient { get; set; }
+
+		public AsyncWebWorker ()
+		{
+			MyWebClient = new WebClient ();
+		}
+
+		public async Task<string> DownloadHtmlStringAsync (string url)
+		{
+			var task = MyWebClient.DownloadStringTaskAsync (url);
+			var result = await task;
+			return result;
+		}
+	}
+
+}
